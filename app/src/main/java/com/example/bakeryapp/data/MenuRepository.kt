109abe1858d2cebc6bcc4e679619a98a6e03ee74
Nicole Rojas <63315208+NPRojas@users.noreply.github.com>
2025-03-02 20:26:44 -0500

    - prepare to move menu data to assets or... maybe firebase

--- app/src/main/java/com/example/bakeryapp/data/MenuRepository.kt
+++ app/src/main/java/com/example/bakeryapp/data/MenuRepository.kt
@@ -1,29 +1,30 @@
 package com.example.bakeryapp.data
 
+import android.content.Context
 import com.example.bakeryapp.R
+import com.google.firebase.crashlytics.buildtools.reloc.com.google.common.reflect.TypeToken
+import com.google.gson.Gson
+
 
 // This is a mock repository as this is app is meant for front end practice
 
-class MenuRepository {
-
-    private val menuItems = listOf(
-        MenuItem(0, "Iced Coffee", 2.99, R.drawable.coffee),
-        MenuItem(1, "Bagel with Cream Cheese", 4.75, R.drawable.bagel),
-        MenuItem(2, "Croissant Sandwich", 5.50, R.drawable.croissant),
-        MenuItem(3, "Rose Macaron", 2.99, R.drawable.rose_macaron),
-        MenuItem(4, "Chocolate Chip Walnut Cookie", 4.00, R.drawable.chocolate_chip_walnut_cookies),
-        MenuItem(5, "Cream Puff", 5.50, R.drawable.cream_puffs),
-        MenuItem(6, "Apple Cinnamon Danish", 5.25, R.drawable.apple_cinnamon_danish),
-        MenuItem(7, "Raspberry Rugelach", 3.50, R.drawable.raspberry_rugelach),
-        MenuItem(8, "Blueberry Scone", 4.25, R.drawable.blueberry_scones)
-    )
+class MenuRepository(private val context: Context) {
+    private val gson = Gson()
 
     val currentOrder = mutableListOf<OrderItem>()
 
     val processedMenuItems = mutableListOf<String>()
 
+    private fun loadMenuAssets(): String {
+        return context.assets.open("menu.json")
+            .bufferedReader().use {
+                it.readText()
+            }
+    }
+
     fun getMenuItems(): List<MenuItem> {
-        return menuItems
+        val json = loadMenuAssets()
+        return gson.fromJson(json, object : TypeToken<List<MenuItem>>() {}.type)
     }
 
     fun getMenuItemById(itemId: Int): MenuItem {
